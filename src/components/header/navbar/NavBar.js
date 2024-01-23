1fe5af1edf20b6f48e35c9e02cb00635554e7a17
T.K. Bui <62676217+tkbui@users.noreply.github.com>
2024-01-22 23:44:57 -0800

    Add sticky top class to navbar

--- src/components/header/navbar/NavBar.js
+++ src/components/header/navbar/NavBar.js
@@ -9,7 +9,7 @@ function NavBar() {
   const path = usePathname();
 
   return (
-    <nav className="d-flex justify-content-center align-center p-3 mb-3 mw-100 navbar border-bottom border-primary border-2 ">
+    <nav className="d-flex justify-content-center align-center p-3 mb-3 mw-100 navbar border-bottom border-primary border-2 sticky-top">
       <Link href="/" className={`me-5 nav-link ${path === '/' ? 'active' : 'inactive'}`}>
         Home
       </Link>
