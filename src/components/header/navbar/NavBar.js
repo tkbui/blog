53a992319d5809a0ae0c2434ed4f2ad5f98c102c
T.K. Bui <62676217+tkbui@users.noreply.github.com>
2024-01-26 00:04:20 -0800

    Center navbar links

--- src/components/header/navbar/NavBar.js
+++ src/components/header/navbar/NavBar.js
@@ -3,15 +3,18 @@
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import NavLink from './NavLink';
+import Link from 'next/link';
 
 function NavBar() {
   const currentPath = usePathname();
 
   return (
     <nav className="d-flex justify-content-center align-center p-3 mb-3 mw-100 navbar border-bottom border-primary border-2 sticky-top">
-      <NavLink path="/" label="Home" currentPath={currentPath} />
-      <NavLink path="/games" label="Games" currentPath={currentPath} />
-      <NavLink path="/music" label="Music" currentPath={currentPath} />
+      <div className="d-flex flex-row justify-content-center align-center">
+        <NavLink path="/" label="Home" currentPath={currentPath} />
+        <NavLink path="/games" label="Games" currentPath={currentPath} />
+        <NavLink path="/music" label="Music" currentPath={currentPath} />
+      </div>
     </nav>
   );
 }
