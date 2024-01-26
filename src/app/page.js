8ea25f0386b6ee573e136cf8116bd60a64909b93
T.K. Bui <62676217+tkbui@users.noreply.github.com>
2024-01-25 20:22:15 -0800

    Add styling and description on home

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Image from 'next/image';
 
 export default function Home() {
-  const description = '';
+  const description =
+    "i've been learning some frontend (next.js, react.js, html, bootstrap css) at work and i'd like to try to practice and improve on these skills by making a lil blog site! hopefully this will be filled with some tidbits about me - namely hobbies and interests, but maybe in the future i'll add some professional deets too :) i'm also hoping to make certain pages dynamic and pull data via apis e.g. with spotify rather than relying on static data in the project. but that's for future me to worry about!";
   return (
-    <div className="container mt-5">
-      <div className="d-flex justify-content-center flex-row m-3">
-        <div className="d-flex flex-column pe-4">
+    <div className="d-flex flex-column mt-4 mx-auto">
+      <div className="d-flex justify-content-center flex-row m-3 image-header-section">
+        <div className="pe-4">
           <Image
             src="/images/tatami cover square.png"
             width={250}
@@ -20,7 +21,7 @@ export default function Home() {
           <h4 className="text-secondary">and welcome, welcome!!!</h4>
         </div>
       </div>
-      <div className="d-flex flex-row justify-content-center mt-1">
+      <div className="description p-5 pt-3 text-center mx-auto">
         <p>{description}</p>
       </div>
     </div>
